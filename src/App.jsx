ec3bae4786075505d459e2188a011473abaf4dd2
ezapratama-tech/nusajoy <eza.pratama655@sma.belajar.id>
2026-09-11 16:08:43 +0700

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ const Explore = lazy(() =>
 )
 
 const Recommendation = lazy(() =>
-  import('./pages/recommendation')
+  import('./pages/Recommendation')
 )
 
 const DestinationDetail = lazy(() =>
